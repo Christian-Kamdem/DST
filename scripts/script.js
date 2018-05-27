3dd872611501f00f8cb2782f08c9ec8c3e0caecf
Christian Kamdem <christiankamdem2@gmail.com>
2018-05-27 17:48:27 +0100

    The first design

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+//On vérifie que le DOM est bien chargé
+document.addEventListener("DOMContentLoaded",()=>{init();}
+,false);
+
+function init(){
+	const semaine = document.getElementById("semaine");
+	const vap = document.getElementById("vap");
+	const vir = document.getElementById("vir");
+}
