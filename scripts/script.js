6aba15750e82bc1a17a5938a7354f6823ec2fb5e
Christian Kamdem <christiankamdem2@gmail.com>
2018-05-28 00:02:50 +0100

    First loop on the bigObject(tab)

--- scripts/script.js
+++ scripts/script.js
@@ -16,11 +16,68 @@ function init(){
 		{Septembre:[462,505,54,706,684,202,66,1080]},
 		{Octobre:[355,455,57,602,610,197,72,929]},
 		{Novembre:[120,171,41,267,274,145,40,441]},
-		{Decembre:[]}
+		{Decembre:[46,64,22,104,117,67,36,221]}
 	];
-
-	const semaine = document.getElementById("semaine");
-	const vap = document.getElementById("vap");
-	const vir = document.getElementById("vir");
+	
 	const send = document.getElementById("submit");
+	//
+	send.addEventListener("click",()=>{
+		const semaine = document.getElementById("semaine").value;
+		const vap = document.getElementById("vap").value;
+		const vir = document.getElementById("vir").value;
+		//On vériffie que tout les champs sont non vides
+		if(semaine === "" || vap === "" || vir === ""){
+			alert("Veuillez remplir tout les champs!");	
+		}else{
+			//On vérifie la validité des valeurs entrées
+			if(Number.isInteger(semaine)){
+				alert("Erreur sur le type de valeur entree!");
+				}else{
+					let i = monthNumber(semaine)-1;
+					while(i<tableau.length){
+						console.log(tableau[i]);
+						i++;
+					}
+				}
+		}		
+	},false);
+function monthNumber(semaine){
+	if(semaine>=1 && semaine<5){
+		//Décembre
+		return 1;
+	}else if(semaine>=5 && semaine<9){
+		//Janvier
+		return 2;
+	}else if(semaine>=9 && semaine<13){
+		//Février
+		return 3;
+	}else if(semaine>=13 && semaine<17){
+		//Mars
+		return 4;
+	}else if(semaine>=17 && semaine<21){
+		//Avril
+		return 5;
+	}else if(semaine>=21 && semaine<25){
+		//Mai
+		return 6;
+	}else if(semaine>=25 && semaine<30){
+		//Juin
+		return 7;
+	}else if(semaine>=30 && semaine<34){
+		//Juillet
+		return 8;
+	}else if(semaine>=34 && semaine<38){
+		//Aout
+		return 9;
+	}else if(semaine>=38 && semaine<42){
+		//Septembre
+		return 10;
+	}else if(semaine>=42 && semaine<46){
+		//Octobre
+		return 11;
+	}else if(semaine>=46 && semaine<50){
+		//Novembre
+		return 12;
+	}
+}
 }
